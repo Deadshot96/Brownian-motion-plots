c21a597a269a47fe317324a0f64544b848a56923
Bhushan <bhushan1810patil@gmail.com>
2021-03-27 02:08:41 +0530

    tweaked many things n worked well

--- Brownian-kD.py
+++ Brownian-kD.py
@@ -1,32 +1,36 @@
 import numpy as np
+from typing import List
 
 """
     Brownian Motion K dimension.
 """
-def BrowniankD(t0: float = 0, t1: float = 1., N: int = 10000, k: int = 4, nums: int = 3):
+def BrowniankD(t0: float = 0, t1: float = 1., N: int = 10000, ndim: int = 4, nums: int = 3, mean: List[float] = (0)):
     
+    if not isinstance(mean, (tuple, list)) or len(mean) != ndim:
+        mean = [0] * ndim
+
     timeSpace = np.linspace(t0, t1, N, endpoint=True, dtype=np.float64)
     dt = timeSpace[1] - timeSpace[0]
 
-    mean = [0] * k
     mean = np.array(mean, dtype=np.float64)
     mean = np.expand_dims(mean, 0)
     mean = mean.repeat(nums, axis=0)
     mean = np.expand_dims(mean, 0)
 
-    stdDev = [dt] * k
-    stdDev = np.sqrt(np.array(stdDev, dtype=np.float64))
-    stdDev = np.expand_dims(stdDev, 0)
+    std = np.sqrt(np.array([dt] * ndim, dtype=np.float64))
+    std = np.expand_dims(std, 0)
+    std = std.repeat(nums, 0)
 
-    # print(mean.shape)
-    # print(stdDev)
+    print(mean.shape)
+    print(std.shape)
 
-    dB = stdDev * np.random.normal(size=(N - 1, nums, k))
+    dB = std * np.random.normal(size=(N - 1, nums, ndim))
     
-    B = np.cumsum(dB, axis=0, dtype=np.float64)
+    B = np.cumsum(dB, axis=0, dtype=np.float64) + mean
     B = np.concatenate((mean, B))
     
     return B
     
 if __name__ == "__main__":
-    BrowniankD(0, 1., 10000, 4, 3)
+    Data = BrowniankD(t0=0, t1=1., N=10000, ndim=4, nums=3, mean=(3, 4, 1, 3))
+    print(Data)
